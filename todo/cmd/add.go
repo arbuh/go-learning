10b393d8c28d2ce202d2d4045dd462ae224992d4
Pavels <p.smirnov@outlook.com>
2025-08-01 17:09:30 +0200

    Process arguments by the add command

--- todo/cmd/add.go
+++ todo/cmd/add.go
@@ -12,8 +12,9 @@ func createAddCmd(appConext *app.Context) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "add",
 		Short: "Add a new task",
+		Args: cobra.MatchAll(cobra.ExactArgs(1)),
 		Run: func(cmd *cobra.Command, args []string) {
-			task := &domain.Task{Description: "do the things"}
+			task := &domain.Task{Description: args[0]}
 			appConext.TaskRepository.Add(task)
 		},
 	}
