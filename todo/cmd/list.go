c89df938455b5a9298288fb706c916073c42fff8
Pavels <p.smirnov@outlook.com>
2025-08-07 15:00:52 +0200

    Implement no current task message

--- todo/cmd/list.go
+++ todo/cmd/list.go
@@ -19,13 +19,18 @@ func createListCmd(appConext *app.Context) *cobra.Command {
 		Use:   "list",
 		Short: "List all tasks",
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("Current tasks:")
-
 			tasks := appConext.TaskRepository.GetAll()
-			for i, task := range tasks {
-				date := task.CreatedAt.Format(dateFormat)
 
-				fmt.Printf("%d. %s, from %s\n", i, task.Description, date)
+			if len(tasks) == 0 {
+				fmt.Println("No current tasks")
+			} else {
+				fmt.Println("Current tasks:")
+
+				for i, task := range tasks {
+					date := task.CreatedAt.Format(dateFormat)
+
+					fmt.Printf("%d. %s, from %s\n", i, task.Description, date)
+				}
 			}
 		},
 	}
